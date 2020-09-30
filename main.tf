5c35ffb432126a2f5af18a7e91deb131d1da54ca
Cody De Arkland <cody@hashicorp.com>
2020-09-30 01:03:10 -0700

    adding configs for app and ingress

--- main.tf
+++ main.tf
@@ -9,3 +9,32 @@ resource "consul_intention" "fe-igw-allow" {
     destination_name = "frontend"
     action           = "allow"
   }
+
+resource "consul_config_entry" "frontend" {
+      name = "frontend"
+      kind = "service-defaults"
+
+      config_json = jsonencode({
+        Protocol    = "http"
+      })
+    }
+
+resource "consul_config_entry" "ingress" {
+      kind = "ingress-gateway"
+      name = "ingress-gateway"
+
+      config_json = jsonencode({
+        Listeners = [
+     {
+       Port = 8080
+       Protocol = "http"
+       Services = [
+         {
+           Name = "frontend"
+           Hosts = ["*"]
+         }
+       ]
+     }
+    ]
+    })
+    }
