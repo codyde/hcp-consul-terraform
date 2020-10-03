b4fee4ffd845cafad518250c540070f1d8e82328
Cody De Arkland <codydearkland@gmail.com>
2020-10-03 15:49:32 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,35 +38,6 @@ resource "consul_intention" "api-db-allow" {
         action           = "allow"
       }
 
-resource "consul_config_entry" "frontend" {
-      name = "frontend"
-      kind = "service-defaults"
-
-      config_json = jsonencode({
-        Protocol    = "http"
-      })
-    }
-
-resource "consul_config_entry" "ingress" {
-      kind = "ingress-gateway"
-      name = "ingress-gateway"
-
-      config_json = jsonencode({
-        Listeners = [
-     {
-       Port = 8080
-       Protocol = "http"
-       Services = [
-         {
-           Name = "frontend"
-           Hosts = ["*"]
-         }
-       ]
-     }
-    ]
-    })
-    }
-
 resource "consul_config_entry" "terminating" {
   kind = "terminating-gateway"
   name = "terminating-gateway"
