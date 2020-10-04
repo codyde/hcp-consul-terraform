39013e40d867f0f2a0db56300a493f7e29ef541c
Cody De Arkland <codydearkland@gmail.com>
2020-10-03 21:57:13 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "consul_service" "hcpgsql" {
 
 resource "consul_node" "hcpgsql" {
   name    = "hcpgsql"
-  address = "postgres.cbjb8qnvkuc9.us-west-2.rds.amazonaws.com"
+  address = "hcpgsql.cbjb8qnvkuc9.us-west-2.rds.amazonaws.com"
   meta = {
     "external-node" = "true"
     "external-probe" = "true"
