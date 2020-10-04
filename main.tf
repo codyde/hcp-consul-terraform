2ed03f0ac210ea5fe2b5b5fb250f57ef2a6c02f9
Cody De Arkland <codydearkland@gmail.com>
2020-10-04 00:47:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "consul_service" "hcpgsql" {
 
 resource "consul_node" "awsrdspg" {
   name    = "awsrdspg"
-  address = "172.31.36.61"
+  address = "hcpgsql.cbjb8qnvkuc9.us-west-2.rds.amazonaws.com"
   meta = {
     "external-node" = "true"
     "external-probe" = "true"
@@ -45,28 +45,24 @@ resource "consul_config_entry" "frontend" {
   config_json = jsonencode({
     Protocol    = "http"
   })
-}
+  }
 
 resource "consul_config_entry" "ingress" {
-      depends_on = [consul_config_entry.frontend]
-      kind = "ingress-gateway"
-      name = "ingress-gateway"
+    depends_on = [consul_config_entry.frontend]
+    kind = "ingress-gateway"
+    name = "ingress-gateway"
 
-      config_json = jsonencode({
-        Listeners = [
-     {
-       Port = 8080
-       Protocol = "http"
-       Services = [
-         {
-           Name = "frontend"
-           Hosts = ["*"]
-         }
-       ]
-     }
-    ]
+    config_json = jsonencode({
+        Listeners = [{
+          Port = 8080
+          Protocol = "http"
+          Services = [{
+            Name = "frontend"
+            Hosts = ["*"]
+          }]}
+        ]
     })
-    }
+  }
 
 
 resource "consul_config_entry" "terminating_gateway" {
