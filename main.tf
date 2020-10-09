bea5ccdbb40dde52b81bc2fbc02c0215456094dc
Cody De Arkland <cody@hashicorp.com>
2020-10-09 14:36:04 -0700

    new api and frontend and splitters

--- main.tf
+++ main.tf
@@ -74,4 +74,84 @@ resource "consul_config_entry" "terminating_gateway" {
     })
   }
 
+resource "consul_config_entry" "api" {
+  name = "api"
+  kind = "service-defaults"
+
+  config_json = jsonencode({
+    Protocol    = "http"
+  })
+  }
+
+resource "consul_config_entry" "api_resolver" {
+  kind = "service-resolver"
+  name = consul_config_entry.api.name
+
+  config_json = jsonencode({
+    DefaultSubset = "v1"
+
+    Subsets = {
+      "v1" = {
+        Filter = "Service.Meta.version == v1"
+      }
+      "v2" = {
+        Filter = "Service.Meta.version == v2"
+      }
+    }
+  })
+}
+
+resource "consul_config_entry" "api_splitter" {
+  kind = "service-splitter"
+  name = consul_config_entry.api_resolver.name
+
+  config_json = jsonencode({
+    Splits = [
+      {
+        Weight        = 50
+        ServiceSubset = "v1"
+      },
+      {
+        Weight        = 50
+        ServiceSubset = "v2"
+      },
+    ]
+  })
+}
+
+resource "consul_config_entry" "frontend_resolver" {
+  kind = "service-resolver"
+  name = consul_config_entry.frontend.name
+
+  config_json = jsonencode({
+    DefaultSubset = "v1"
+
+    Subsets = {
+      "v1" = {
+        Filter = "Service.Meta.version == v1"
+      }
+      "v2" = {
+        Filter = "Service.Meta.version == v2"
+      }
+    }
+  })
+}
+
+resource "consul_config_entry" "frontend_splitter" {
+  kind = "service-splitter"
+  name = consul_config_entry.frontend_resolver.name
+
+  config_json = jsonencode({
+    Splits = [
+      {
+        Weight        = 0
+        ServiceSubset = "v1"
+      },
+      {
+        Weight        = 100
+        ServiceSubset = "v2"
+      },
+    ]
+  })
+}
 
