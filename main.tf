adeddf7cc5b8383f19f70a5f020e52228165881d
Cody De Arkland <cody@hashicorp.com>
2020-10-09 14:06:49 -0700

    initial commit

--- main.tf
+++ main.tf
@@ -4,42 +4,4 @@ provider "consul" {
   token      = var.token
 }
 
-resource "consul_intention" "fe-igw-allow" {
-    source_name      = "ingress-gateway"
-    destination_name = "frontend"
-    action           = "allow"
-  }
-
-resource "consul_intention" "fe-api-allow" {
-      source_name      = "frontend"
-      destination_name = "api"
-      action           = "allow"
-    }
-
-resource "consul_config_entry" "frontend" {
-  name = "frontend"
-  kind = "service-defaults"
-
-  config_json = jsonencode({
-    Protocol    = "http"
-  })
-  }
-
-resource "consul_config_entry" "ingress" {
-    depends_on = [consul_config_entry.frontend]
-    kind = "ingress-gateway"
-    name = "ingress-gateway"
-
-    config_json = jsonencode({
-        Listeners = [{
-          Port = 80
-          Protocol = "http"
-          Services = [{
-            Name = "frontend"
-            Hosts = ["*"]
-          }]}
-        ]
-    })
-  }
-
 
